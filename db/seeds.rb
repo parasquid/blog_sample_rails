7ba5b281ae7c85a9a8aebb400b4df3ea6f3cc042
parasquid <tristan.gomez@gmail.com>
2017-12-30 03:26:11 +0800

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+40.times do
+  matz = ->{ Faker::Matz.unique.quote }
+  one_piece = ->{ Faker::OnePiece.unique.quote }
+  star_wars = ->{ Faker::StarWars.unique.quote }
+
+  article = Article.create(title: Faker::Lorem.unique.sentence, text: [matz, one_piece, star_wars].sample.call)
+  puts article.inspect
+  rand(5..20).times do
+    comment = article.comments.create(commenter: Faker::Witcher.character, body: Faker::Hacker.say_something_smart)
+    puts comment.inspect
+  end
+end
