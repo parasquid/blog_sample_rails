95870892bad7ba9d5b11b0a4bb845267dd37e6af
parasquid <tristan.gomez@gmail.com>
2018-01-01 02:45:06 +0800

    componetize the about and footer
    we've removed the dependency on the included vuejs script and started to treat the components as individual javascript applications
    
    there's also the benefit of being able to use javascript string literals (see the about component)
    
    one important caveat: .vue files are not erb files (we're not in ruby land anymore) so custom erb will need to be rewritten to their js counterparts

---
+++ app/javascript/packs/apps/about_page/App.vue
@@ -0,0 +1,23 @@
+<template functional>
+<div>
+  <h4 name="60df" class="graf--p">Introducing Official Bootstrap themes from&nbsp;the founders of Bootstrap.</h4>
+  <p name="d493" class="graf--p">In the last four years we’ve spent thousands of hours designing and developing Bootstrap and other open source projects for millions of people and sites across the web. In that time we’ve also helped to build some amazing products like Twitter, GitHub, Medium, and more. Now, we’re hoping to do it with our own business by creating and selling high quality Bootstrap themes.</p>
+  <p name="d9d4" class="graf--p">To start, we’re launching with three themes, one for each of the most popular types of Bootstrapped sites — dashboards, applications, and marketing sites. Each theme includes:</p>
+  <ul class="postList">
+  <li name="d762" class="graf--li">Beautiful example pages</li>
+  <li name="8ffb" class="graf--li">Custom documentation</li>
+  <li name="daaf" class="graf--li">Several brand new, custom components</li>
+  <li name="e5dc" class="graf--li">Source Less and JavaScript files</li>
+  <li name="bece" class="graf--li">Easy build tools with Gulp</li>
+  </ul>
+  <p name="243f" class="graf--p">We’ve designed our themes as toolkits, just like Bootstrap, to provide you with a highly functional, visually consistent, and beautiful set of components.</p>
+  <p name="6389" class="graf--p">While Bootstrap is free for everyone, these themes are only available for purchase. We decided to sell them for a few reasons:</p>
+  <ul class="postList">
+  <li name="89a7" class="graf--li">We think they’re worth it. Each of us has years of experience building for the web — from products to frameworks — and we’ve poured all of that into these themes.</li>
+  <li name="81fc" class="graf--li">We keep Bootstrap funded and available for everyone. To date, we have never taken money from investors and have no corporate sponsorships. All our expenses are paid for by us through ad revenue on our docs.</li>
+  <li name="1f3d" class="graf--li">We can give more back to the Bootstrap and open source communities with event sponsorships, swag, and more open source projects.</li>
+  </ul>
+  <p name="857d" class="graf--p">Bootstrap’s entire mission is to help awesome people do awesome stuff. It’s been our goal since day one and we’re excited to take that to another level with Bootstrap Themes.</p>
+  <p name="1794" class="graf--p">❤,<br><a href="https://github.com/mdo" data-href="https://github.com/mdo" class="markup--anchor markup--p-anchor">@mdo</a>, <a href="https://github.com/fat" data-href="https://github.com/fat" class="markup--anchor markup--p-anchor">@fat</a>, and <a href="https://github.com/dhg" data-href="https://github.com/dhg" class="markup--anchor markup--p-anchor">@dhg</a></p>
+</div>
+</template>
---
+++ app/javascript/packs/apps/footer/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="py-4 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">{{ message }}</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: `Copyright © My Rails Blog ${new Date().getFullYear()}`
+    };
+  }
+};
+</script>
